2345b09b0c382b540559bc0f177613f18e56a3c1
vatsal-dp <ptelvtsl@gmail.com>
2024-10-26 18:17:54 -0400

    separate backend and frontend

--- src/main.py
+++ src/main.py
@@ -20,46 +20,7 @@ import sys
 import os
 sys.path.append(os.path.abspath("Slash"))
 
-import streamlit as st
-from streamlit_option_menu import option_menu
 
-import frontend.account as account
-import frontend.slash_user_interface as slash_user_interface
-
-st.set_page_config(page_title='ShopSync', layout= "wide")
-
-class MultiApp:
-    def __init__(self):
-        self.apps = []
-    
-    def add_app(self,title,function):
-        self.apps.append({
-            "title":title,
-            "function": function
-        })
-    
-    def run():
-        with st.sidebar:
-            app = option_menu(
-                menu_title='ShopSync',
-                options=['Account', 'Home', 'Favourites','Logout'],
-                icons=['person-circle', 'house-fill', 'star-fill', 'box-arrow-right'],
-                menu_icon='chat-text-fill',
-                default_index=1,
-                styles={
-                    "container": {"padding": "5!important","background-color":'black'},
-        "icon": {"color": "white", "font-size": "23px"}, 
-        "nav-link": {"color":"white","font-size": "20px", "text-align": "left", "margin":"0px", "--hover-color": "blue"},
-        "nav-link-selected": {"background-color": "#02ab21"},}
-            )
-        
-        if app=="Account":
-            account.app()
-        
-        if app=="Home":
-            slash_user_interface.app()
-    
-    run()
 # local imports
 import scraper as scr
 
